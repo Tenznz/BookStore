a8194dff3fb6236f81a3589e93a445a55abd92e5
tenznz <dhugkar@gmail.com>
2022-03-31 00:10:12 +0530

    Added: User signin and signup

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from user import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, user_id, username, email, password):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f"User('{self.user_id}', '{self.username}', '{self.email}', '{self.password}')"
